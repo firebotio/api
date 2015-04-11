383fc538c25e7a9ee8ae011600fecd9415cd11b1
Tommy Dang <quantumventuress@gmail.com>
2015-04-11 07:53:33 -0700

    added application id to session and access token

--- app/models/access_token.rb
+++ app/models/access_token.rb
@@ -1,25 +1,30 @@
 class AccessToken
   attr_reader :expires_at, :tokenable_id, :tokenable_type
 
-  def initialize(token)
-    result = find_by_token token
+  def initialize(options = {})
+    result = find_by_token options[:application_key]
     if result
+      @application_id = options[:application_id]
       @expires_at     = result["expires_at"].try :to_datetime
       @tokenable_id   = result["tokenable_id"]
       @tokenable_type = result["tokenable_type"]
+      @tokenable_uid  = result["tokenable_uid"]
     end
   end
 
   def error
     if expires_at.nil? || tokenable_id.nil? || tokenable_type.nil?
       "invalid or missing"
+    elsif !valid_application_id
+      "invalid application id"
     elsif expired?
       "expired"
     end
   end
 
   def valid?
-    expires_at && tokenable_id && tokenable_type && !expired?
+    expires_at && tokenable_id && tokenable_type &&
+    valid_application_id && !expired?
   end
 
   private
@@ -29,12 +34,16 @@ class AccessToken
   end
 
   def fields
-    "expires_at, tokenable_id, tokenable_type"
+    "expires_at, tokenable_id, tokenable_type, tokenable_uid"
   end
 
   def find_by_token(token)
     ActiveRecord::Base.connection.exec_query(
       "SELECT #{fields} FROM Tokens WHERE token = '#{token}' LIMIT 1;"
     ).first
+  end
+
+  def valid_application_id
+    @application_id == @tokenable_uid
   end
 end
--- app/models/session.rb
+++ app/models/session.rb
@@ -1,17 +1,25 @@
 class Session
-  HEADER = "Firebot-Authorization"
+  HEADER_APPLICATION_KEY = "X-Firebot-Application-Key"
+  HEADER_APPLICATION_ID  = "X-Firebot-Application-Id"
 
   def initialize(request)
     @request = request
   end
 
   def access_token
-    @access_token ||= AccessToken.new(token)
+    @access_token ||= AccessToken.new(
+      application_key: application_key,
+      application_id:  application_id
+    )
   end
 
   private
 
-  def token
-    @request.headers[HEADER]
+  def application_key
+    @request.headers[HEADER_APPLICATION_KEY]
+  end
+
+  def application_id
+    @request.headers[HEADER_APPLICATION_ID]
   end
 end
